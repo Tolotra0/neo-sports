a3cae92dcf679b9350ac5a63b6bd4774efdc1c90
Tolotra0 <tolotra.and@gmail.com>
2019-03-08 09:46:33 +0300

    Game stats API: Added team filter in seasons' games.

--- main_api/game_stats.py
+++ main_api/game_stats.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from models.db_engine import engine
 from sqlalchemy.sql import text
 
+
 game_stats_api = Blueprint('game_stats_api', __name__)
 
 
@@ -36,12 +37,13 @@ def get_all():
     return jsonify([dict(row) for row in result])
 
 
-# EXAMPLE: /api/games/CLE/2016/12/25
+# EXAMPLE: /api/games/team/CLE/2016/12/25
 # -> CLE game for 25 December 2016
-@game_stats_api.route('/<string:team>', methods=['GET'])
-@game_stats_api.route('/<string:team>/<int:year>', methods=['GET'])
-@game_stats_api.route('/<string:team>/<int:year>/<int:month>', methods=['GET'])
-@game_stats_api.route('/<string:team>/<int:year>/<int:month>/<int:day>', methods=['GET'])
+#
+@game_stats_api.route('/team/<string:team>', methods=['GET'])
+@game_stats_api.route('/team/<string:team>/<int:year>', methods=['GET'])
+@game_stats_api.route('/team/<string:team>/<int:year>/<int:month>', methods=['GET'])
+@game_stats_api.route('/team/<string:team>/<int:year>/<int:month>/<int:day>', methods=['GET'])
 def teams(team, year=None, month=None, day=None):
     conn = engine.connect()
     s = None
@@ -61,7 +63,7 @@ def teams(team, year=None, month=None, day=None):
         s = s + ' AND DAY(Games.Date) = :day '
 
     if s is None:
-        return jsonify({'result': None})
+        return jsonify(None)
 
     s = text(s + sorting)
 
@@ -78,17 +80,41 @@ def teams(team, year=None, month=None, day=None):
     return jsonify([dict(row) for row in result])
 
 
-@game_stats_api.route('/season/<int:begin_year>')
-def season(begin_year):
+# EXAMPLES:
+# /api/games/season/2017
+# -> 2017-2018 season's games
+# /api/games/season/2017/TOR
+# -> 2017-2018 season's games for Toronto Raptors
+#
+@game_stats_api.route('/season/<int:begin_year>', methods=['GET'])
+@game_stats_api.route('/season/<int:begin_year>/<string:team>', methods=['GET'])
+def season(begin_year, team=None):
     conn = engine.connect()
 
-    s = text(base_game_request +
-             ' WHERE (YEAR(Games.Date) = :beginYear AND MONTH(Games.Date) >= :beginMonth) \
-              OR (YEAR(Games.Date) = :endYear AND MONTH(Games.Date) <= :endMonth) '
-             + sorting)
+    result = None
 
-    result = conn.execute(s, beginYear=begin_year, beginMonth=season_start_month,
-                          endYear=begin_year+1, endMonth=season_end_month)
+    s = base_game_request + \
+        ' WHERE ((YEAR(Games.Date) = :beginYear AND MONTH(Games.Date) >= :beginMonth) \
+        OR (YEAR(Games.Date) = :endYear AND MONTH(Games.Date) <= :endMonth)) '
+
+    if team is None:
+        s = text(s + sorting)
+        result = conn.execute(s, beginYear=begin_year, beginMonth=season_start_month,
+                              endYear=begin_year + 1, endMonth=season_end_month)
+
+    else:
+        team_id = conn.execute(text('SELECT Id FROM Teams WHERE ShortName = :x'), x=team).fetchone()
+
+        if team_id is not None:
+            team_id = team_id['Id']
+            s = text(s + ' AND Team.Id = :id ' + sorting)
+
+            result = conn.execute(s, beginYear=begin_year, beginMonth=season_start_month,
+                                  endYear=begin_year + 1, endMonth=season_end_month, id=team_id)
 
     conn.close()
+
+    if result is None:
+        return jsonify(None)
+
     return jsonify([dict(row) for row in result])
