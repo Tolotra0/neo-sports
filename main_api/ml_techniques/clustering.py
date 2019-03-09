bb4ab3faf581b2c0dfc8c06a8cea6e56c02c02af
Tolotra0 <tolotra.and@gmail.com>
2019-03-09 18:11:50 +0300

    Added teams Clustering.

--- main_api/ml_techniques/clustering.py
+++ main_api/ml_techniques/clustering.py
@@ -1,7 +1,45 @@
+#
+# API for getting different sorts of clustering like:
+# teams clustering, players clustering, and so on ...
+#
+
 from flask import Blueprint, jsonify
-import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
+from main_api._dataframes import get_all_teams_stats_aggregation
+from main_api._common_requests_parameters import visualization_columns
+
 
 clustering_api = Blueprint('clustering_api', __name__)
+
+
+# API for getting teams clustering based on multiple parameters
+# EXAMPLE: /main/clustering/teams/3/2016
+# -> Get the teams' clustering based by stats, for 2016-2017 season, k=3
+#
+@clustering_api.route('/teams', methods=['GET'])
+@clustering_api.route('/teams/<int:k>/<int:season_begin_year>', methods=['GET'])
+@clustering_api.route('/teams/<int:k>/<int:season_begin_year>/<string:conference>', methods=['GET'])
+def teams_clustering(k=4, season_begin_year=None, conference=None):
+    df = get_all_teams_stats_aggregation(season_begin_year=season_begin_year, conference=conference)
+
+    if df.empty:
+        return jsonify(None)
+
+    print(df)
+
+    teams = df['TeamShortName']
+
+    df = df[visualization_columns]
+
+    pca = PCA(n_components=2)
+    pca.fit(df)
+
+    points_2d = pca.transform(df)
+
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(points_2d)
+    cluster_labels = kmeans.labels_
+
+    return jsonify(teams.tolist(), points_2d.tolist(), cluster_labels.tolist())
--- main_api/ml_techniques/visualization.py
+++ main_api/ml_techniques/visualization.py
@@ -4,9 +4,7 @@
 #
 
 from flask import Blueprint, jsonify
-import pandas as pd
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
 
 from main_api._dataframes import get_all_teams_stats_aggregation, get_one_team_stats_aggregation
 from main_api._common_requests_parameters import visualization_columns
@@ -23,9 +21,6 @@ visualization_api = Blueprint('visualization_api', __name__)
 @visualization_api.route('/points/teams/<int:season_begin_year>', methods=['GET'])
 @visualization_api.route('/points/teams/<int:season_begin_year>/<string:conference>', methods=['GET'])
 def points_teams(season_begin_year=None, conference=None):
-    if conference is not None and conference.lower() == 'all':
-        conference = None
-
     df = get_all_teams_stats_aggregation(season_begin_year=season_begin_year, conference=conference)
 
     if df.empty:
