1fc2e18b65bb95a58163d85a9bcc81363942d39e
Tolotra0 <tolotra.and@gmail.com>
2019-03-12 13:43:02 +0300

    commit 1

--- main_api/basic_data/season_stats.py
+++ main_api/basic_data/season_stats.py
@@ -29,6 +29,19 @@ def games_average(season_begin_year, conference=None):
     return _season_team_aggregate(season_begin_year, conference, method='MEAN')
 
 
+@season_stats_api.route('/<int:season_begin_year>/to/<int:season_begin_year2>/avg', methods=['GET'])
+@season_stats_api.route('/<int:season_begin_year>/to/<int:season_begin_year2>/avg/<string:conference>', methods=['GET'])
+def multiple_games_average(season_begin_year, season_begin_year2, conference=None):
+    dataframes = []
+    for date in range(season_begin_year, season_begin_year2+1):
+        df = get_all_teams_stats_aggregation(season_begin_year, conference=conference, method='MEAN')
+        dataframes.append({
+            'season': '{}-{}'.format(date, date + 1),
+            'data': df.to_dict(orient='records')
+        })
+    return jsonify(dataframes)
+
+
 # Get the SUM of total stats for each team in one season
 # EXAMPLE: /main/season/2017/sum/east
 # -> Get the SUM of total stats for each team in 2017-2018 season,
